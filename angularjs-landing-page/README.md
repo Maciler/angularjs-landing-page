b176bc4ed3823a8869a74633e7041c7b2daee282
Maciler <macileralmondsy@gmail.com>
2021-07-28 14:03:17 +0300

    Initial Commit

---
+++ angularjs-landing-page/README.md
@@ -0,0 +1,3 @@
+Angularjs Landing Page 
+
+An example of a little app made with AngularJS and Bootstrap. A useful and light landing page
---
+++ angularjs-landing-page/partials/app.js
@@ -0,0 +1,16 @@
+var ourApp = angular.module('landingpage', ["ngRoute", "ngResource", "pascalprecht.translate"]);
+ourApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+		when('/', {
+			templateUrl: 'partials/index/index.html',
+			controller: 'indexCtrl'
+		}).when('/request', {
+			templateUrl: 'partials/request-list/request-list.html',
+			controller: 'requestCtrl'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+	}
+]);
